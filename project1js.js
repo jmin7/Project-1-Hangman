aa21dfa2e1675ffb79e497833b5671a673c483e4
Jungmin <jungmin.yjm@gmail.com>
2016-11-20 15:37:55 -0500

    updated 11.20

--- project1js.js
+++ project1js.js
@@ -1,158 +1,162 @@
 window.onload = function () {
 
-  var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-        'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-        't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-
+  var words;              //word array
   var word ;              // Selected word
-  var guess ;             // Guess
+  var each ;             // Guess
   var guesses = [ ];      // Stored guesses
   var lives ;             // Lives
   var counter ;           // Count correct guesses
-  var space;              // Number of spaces in word '-'
-  var animate;            // Animate the hangman
 
-  // Get elements
-  var showLives = document.getElementById("mylives");
 
-  // create alphabet ul
-  var buttons = function () {
-    letterButtons = document.getElementById('buttons');
-    letters = document.createElement('ul');
+  var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+        'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+        'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
-    for (var i = 0; i < alphabet.length; i++) {
-      letters.id = 'alphabet';
-      list = document.createElement('li');
-      list.id = 'letter';
-      list.innerHTML = alphabet[i];
-      check();
-      letterButtons.appendChild(letters);
-      letters.appendChild(list);
-    }
+  // Play
+  var play = function () {
+    var words = [
+      "JAVASCRIPT", "HANGMAN", "DEBUGGING", "GAMES",
+      "COMMAND", "FIZZBUZZ"
+      ];
+    word = words[Math.floor(Math.random() * words.length)];
+    counter = 0;
+    lives = 7;
+    createButtons();
+    blank();
+    notice();
   }
 
+  // create alphabet button
+  var createButtons = function () {
+    buttons = document.getElementById('buttons');
+    letters = document.createElement('ul');
+    buttons.appendChild(letters);
+    letters.id = 'alphabet';
+    for (var i = 0; i < alphabet.length; i++) {
+      alpha = document.createElement('li');
+      letters.appendChild(alpha);
+      alpha.innerHTML = alphabet[i];
+      compare();
+    }
+  }
 
 
   // Create guesses ul
-   result = function () {
-    wordHolder = document.getElementById('hold');
-    correct = document.createElement('ul');
-
+   var blank = function () {
+     text = document.getElementById('placeForWord');
+     answer = document.createElement('ul');
     for (var i = 0; i < word.length; i++) {
-      correct.setAttribute('id', 'my-word');
-      guess = document.createElement('li');
-      guess.setAttribute('class', 'guess');
-      if (word[i] === "-") {
-        guess.innerHTML = "-";
-        space = 1;
-      } else {
-        guess.innerHTML = "_";
+      each = document.createElement('li');
+      each.setAttribute('id', 'selectedletter');
+    if (word[i] !== 0) {
+      each.innerHTML = "_";
       }
-
-      guesses.push(guess);
-      wordHolder.appendChild(correct);
-      correct.appendChild(guess);
+    text.appendChild(answer);
+    answer.appendChild(each);
+    guesses.push(each);
     }
   }
 
-  // Show lives
-   comments = function () {
-    showLives.innerHTML = "You have " + lives + " lives";
+
+  // left lives / win
+   var notice = function () {
+    var livesLeft = document.getElementById("lives");
+    livesLeft.innerHTML = "You have " + lives + " lives left";
     if (lives < 1) {
-      showLives.innerHTML = "Game Over !! " + " " +" The word was " + word;
-    }
-    for (var i = 0; i < guesses.length; i++) {
-      if (counter + space === guesses.length) {
-        showLives.innerHTML = "You Win!";
+      livesLeft.innerHTML = "Game Over !! " + " " +" The word was " + word;
+      buttons.removeChild(letters);
+      letters.removeChild(alpha);
+      startButton = document.getElementById('start');
+      startButton.innerHTML = "Try Again !"
       }
+
+    for (var i = 0; i < guesses.length; i++) {
+      if (counter === guesses.length) {
+      livesLeft.innerHTML = "You Win !";
+      buttons.removeChild(letters);
+      letters.removeChild(alpha);
+      startButton = document.getElementById('start');
+      startButton.innerHTML = "Try Again !"
+    }
     }
   }
+
 
 
   // OnClick Function
-   check = function () {
-    list.onclick = function () {
-      var geuss = (this.innerHTML);
-      this.setAttribute("class", "active");
-      this.onclick = null;
+  var compare = function () {
+    alpha.onclick = function () {
+     var letterClicked = this.innerHTML;
+     this.setAttribute('id', 'click');
+     this.onclick = null;
       for (var i = 0; i < word.length; i++) {
-        if (word[i] === geuss) {
-          guesses[i].innerHTML = geuss;
-          counter += 1;
+       if (word[i] === letterClicked) {
+        guesses[i].innerHTML = letterClicked;
+        counter ++;
+        notice();
         }
       }
-      var j = (word.indexOf(geuss));
+     var j = (word.indexOf(letterClicked));
       if (j === -1) {
-        lives -= 1;
-        comments();
-
-    // Animate hangman
-        animate = function () {
-        if (lives === 6) {
-      document.getElementById('image').src = 'image/hangman2.png';
-      }
-      else if (lives === 5) {
-      document.getElementById('image').src = 'image/hangman3.png';
-      }
-      else if (lives === 4) {
-      document.getElementById('image').src = 'image/hangman4.png';
-      }
-      else if (lives === 3) {
-      document.getElementById('image').src = 'image/hangman5.png';
-      }
-      else if (lives === 2) {
-      document.getElementById('image').src = 'image/hangman6.png';
-      }
-      else if (lives === 1) {
-      document.getElementById('image').src = 'image/hangman7.png';
-      }
-      else if (lives === 0) {
-      document.getElementById('image').src = 'image/hangman8.png';
-      }
-      }
-        animate();
-      } else {
-        comments();
+       lives --;
+       animate();
+       notice();
       }
     }
   }
 
 
-  // Play
-  play = function () {
-    var words = [
-    "javascript",
-    "hangman",
-    "debugging",
-    "games",
-    "command",
-    "fizzbuzz"
-    ];
+  var animate = function () {
+    if (lives === 6) {
+      document.getElementById('image').src ='image/hangman2.png';
+    }
+    else if (lives === 5) {
+      document.getElementById('image').src = 'image/hangman3.png';
+    }
+    else if (lives === 4) {
+      document.getElementById('image').src = 'image/hangman4.png';
+    }
+    else if (lives === 3) {
+      document.getElementById('image').src = 'image/hangman5.png';
+    }
+    else if (lives === 2) {
+      document.getElementById('image').src = 'image/hangman6.png';
+    }
+    else if (lives === 1) {
+      document.getElementById('image').src = 'image/hangman7.png';
+    }
+    else if (lives === 0) {
+      document.getElementById('image').src = 'image/hangman8.png';
+    }
+    }
 
-    word = words[Math.floor(Math.random() * words.length)];
-    word = word.replace(/\s/g, "-");
-    console.log(word);
-    buttons();
+
+// game start
+play();
+
+
+   // start or play again
+   document.getElementById('start').onclick = function() {
+    if (lives < 1 || counter === guesses.length) {
+      text.removeChild(answer);
+      startButton = document.getElementById('start');
+      startButton.innerHTML = "New Game"
+
+      document.getElementById('image').src ='image/hangman1.png';
+      guesses = [ ];
+      play();
+    }
+
+  else {
+    buttons.removeChild(letters);
+    letters.removeChild(alpha);
+
+    text.removeChild(answer);
+    document.getElementById('image').src ='image/hangman1.png';
 
     guesses = [ ];
-    lives = 7;
-    counter = 0;
-    space = 0;
-    result();
-    comments();
-    // canvas();
-  }
-
-  play();
-
-
-   // Reset
-
-   document.getElementById('reset').onclick = function() {
-     correct.parentNode.removeChild(correct);
-     letters.parentNode.removeChild(letters);
-
-     play();
-  }
+    play();
+    }
+}
 }
