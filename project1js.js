b5b3488bc72071a5ec2c12f96511052b3c800d96
Jungmin <jungmin.yjm@gmail.com>
2016-11-21 00:25:07 -0500

    11.20 update2

--- project1js.js
+++ project1js.js
@@ -7,11 +7,22 @@ window.onload = function () {
   var guesses = [ ];      // Stored guesses
   var lives ;             // Lives
   var counter ;           // Count correct guesses
+  var score = null ;
 
 
   var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
         'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
         'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+  showScore = document.getElementById('currentScore');
+  var scoreBoard = document.createElement('p');
+  showScore.appendChild(scoreBoard);
+
+  playerChange = document.getElementById('newPlayer');
+  newGame = document.createElement('button');
+  newGame.setAttribute('id', 'again')
+  playerChange.appendChild(newGame);
+  newGame.innerHTML = "New player";
 
   // Play
   var play = function () {
@@ -46,6 +57,7 @@ window.onload = function () {
    var blank = function () {
      text = document.getElementById('placeForWord');
      answer = document.createElement('ul');
+     answer.setAttribute('id', 'selectedWord');
     for (var i = 0; i < word.length; i++) {
       each = document.createElement('li');
       each.setAttribute('id', 'selectedletter');
@@ -64,24 +76,36 @@ window.onload = function () {
     var livesLeft = document.getElementById("lives");
     livesLeft.innerHTML = "You have " + lives + " lives left";
     if (lives < 1) {
-      livesLeft.innerHTML = "Game Over !! " + " " +" The word was " + word;
+      livesLeft.innerHTML = "Game Over !! <br>"
+      + " The word was " + word;
       buttons.removeChild(letters);
       letters.removeChild(alpha);
       startButton = document.getElementById('start');
       startButton.innerHTML = "Try Again !"
+
+      pushButton = document.getElementById('instruction');
+      pushButton.innerHTML = " ";
       }
 
     for (var i = 0; i < guesses.length; i++) {
       if (counter === guesses.length) {
-      livesLeft.innerHTML = "You Win !";
+      livesLeft.innerHTML = "&#x263A;&#x263A; You Win ! &#x263A;&#x263A;";
+    }
+    }
+    if (counter === guesses.length) {
       buttons.removeChild(letters);
       letters.removeChild(alpha);
       startButton = document.getElementById('start');
       startButton.innerHTML = "Try Again !"
-    }
+
+      pushButton = document.getElementById('instruction');
+      pushButton.innerHTML = " ";
+
+      score++;
+      scoreBoard.innerHTML = "Score : " + score;
+
     }
   }
-
 
 
   // OnClick Function
@@ -143,6 +167,9 @@ play();
       startButton = document.getElementById('start');
       startButton.innerHTML = "New Game"
 
+      pushButton = document.getElementById('instruction');
+      pushButton.innerHTML = "&#8659; Click the alphabet below to guess the word &#8659;";
+
       document.getElementById('image').src ='image/hangman1.png';
       guesses = [ ];
       play();
@@ -153,10 +180,31 @@ play();
     letters.removeChild(alpha);
 
     text.removeChild(answer);
+    pushButton = document.getElementById('instruction');
+    pushButton.innerHTML = "&#8659; Click the alphabet below to guess the word &#8659;";
+
     document.getElementById('image').src ='image/hangman1.png';
 
     guesses = [ ];
+    score = null;
     play();
     }
 }
+
+// start with score 0 , new player
+document.getElementById('newPlayer').onclick = function() {
+text.removeChild(answer);
+pushButton = document.getElementById('instruction');
+pushButton.innerHTML = "&#8659; Click the alphabet below to guess the word &#8659;";
+
+document.getElementById('image').src ='image/hangman1.png';
+showScore.removeChild(scoreBoard);
+
+startButton = document.getElementById('start');
+startButton.innerHTML = "New Game"
+
+guesses = [ ];
+play();
+}
+
 }
